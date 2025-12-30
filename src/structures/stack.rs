2fde14c529e909db0af5c4dfc28f5c5c17906f5f
Shalom <sasbell101@gmail.com>
2025-12-29 19:46:58 -0500

    added improved comments explaining memory ordering for the stack

--- src/structures/stack.rs
+++ src/structures/stack.rs
@@ -2,7 +2,7 @@ use std::sync::atomic::{AtomicPtr, Ordering};
 
 struct Node {
     value : i32,
-    next : * const Node,
+    next : *const Node,
 }
 
 struct Stack {
@@ -15,13 +15,13 @@ impl Stack {
     }
     fn push(& self, value : i32) {
         loop{
-            let old : * mut Node = self.head.load(std::sync::atomic::Ordering::Acquire); //ensures writes to node x in head by thread that initialized x are done before reading head.
-            let new : * mut Node = Box::into_raw(Box::new(Node{value, next : old})); //allocate and initialize new node to push onto the stack
-            if self.head.compare_exchange(old, new, Ordering::Release, Ordering::Relaxed).is_ok() { break; } // release on success to publish, relaxed on failure because no synchronization required.
+            let old : *mut Node = self.head.load(std::sync::atomic::Ordering::Acquire); // Acquire ensures visibility of all writes that happened-before the successful publication of this head pointer.
+            let new : *mut Node = Box::into_raw(Box::new(Node{value, next : old})); //allocate and initialize new node to push onto the stack
+            if self.head.compare_exchange(old, new, Ordering::Release, Ordering::Relaxed).is_ok() { break; } // Release on success to publish the update to other threads, Relaxed on failure because no synchronization required.
         }
     }
 
     // fn pop(& self) -> Option<i32> {
-    //
+    //Box::new(x) allocates space for x on the heap, moves x into that space, and returns an owning pointer (Box<T>) to it.
     // }
 }
