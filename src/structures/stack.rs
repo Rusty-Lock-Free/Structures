083d36b3fdbc6a2d116f562fde21ff5e9d202e04
Shalom <sasbell101@gmail.com>
2025-12-30 01:02:50 -0500

    cleaner, safe implementation

--- src/structures/stack.rs
+++ src/structures/stack.rs
@@ -1,5 +1,5 @@
-use std::ptr;
 use std::sync::atomic::{AtomicPtr, Ordering};
+use std::{mem, ptr};
 
 struct Node {
     value: i32,
@@ -18,25 +18,27 @@ impl Stack {
     }
 
     fn push(&self, value: i32) {
-        // Allocate and initialize new node to push onto the stack
+        // Allocate and initialize thread-local node to push onto the stack
         let mut new_box: Box<Node> = Box::new(Node {
             value,
             next: ptr::null(),
         });
 
+        let new_ptr: *mut Node = &mut *new_box;
+
         loop {
             // Acquire ensures visibility of all writes that happened-before the successful publication of this head pointer.
-            let old: *mut Node = self.head.load(Ordering::Acquire);
-            new_box.next = old;
-
-            let new_ptr = Box::into_raw(new_box); //convert new_box into a raw pointer for CAS
+            let old_ptr: *mut Node = self.head.load(Ordering::Acquire);
+            new_box.next = old_ptr; //Safe write :  'new_box' is thread-local
 
             // Release on success to publish the update to other threads, Relaxed on failure because no synchronization required.
             if self
                 .head
-                .compare_exchange(old, new, Ordering::Release, Ordering::Relaxed)
+                .compare_exchange(old_ptr, new_ptr, Ordering::Release, Ordering::Relaxed)
                 .is_ok()
             {
+                //CAS succeeded: the stack now owns the node. Prevent reclamation by current thread.
+                mem::forget(new_box);
                 break;
             }
         }
