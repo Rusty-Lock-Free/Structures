0e6f361c9caf48884a4e9f45684daa7a25095c2f
One <43485962+c-git@users.noreply.github.com>
2025-12-31 19:24:29 -0500

    chore: change deny to warning

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
-#![deny(missing_docs)]
-#![deny(missing_debug_implementations)]
+#![warn(missing_docs)]
+#![warn(missing_debug_implementations)]
 #![warn(unused_crate_dependencies)]
-// For documenting optional features. See more at <https://c-git.github.io/rust/documentation/>
 #![cfg_attr(docsrs, feature(doc_cfg))]
-#![cfg_attr(test, deny(warnings))]
 #![forbid(unsafe_code)]
 #![doc = include_str!("../README.md")]
 mod structures;
