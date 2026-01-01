fcc6abe945d2701482d4680a2bafbdbcbacb5cf6
One <43485962+c-git@users.noreply.github.com>
2025-12-31 19:44:31 -0500

    feat: allow unsafe
    We will need it to dereference the raw pointer once we do pop

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,5 @@
 #![warn(missing_debug_implementations)]
 #![warn(unused_crate_dependencies)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
-#![forbid(unsafe_code)]
 #![doc = include_str!("../README.md")]
 mod structures;
